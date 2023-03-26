6a8cd375fa1a5f59fd99b361b3a060660d4b27b8
Gabe <gabriel.velasquez0116@gmail.com>
2023-03-26 09:14:18 -0600

    index modified and app js developed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import CardList from './CardList';
+import Searchbox from './Searchbox.js';
+import Scroll  from '.Scroll.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      cats:[],
+      searchfield: ''
+    }
+  }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+      .then(users => this.setState({cats: users}));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+  }
+
+  render(){
+    const { cats, searchfield } = this.state;
+    const filteredCats = this.state.cats.filter(cat => {
+      return cat.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    return !cats.length ? 
+    <h1>Loading...Please wait. Meow...</h1>:
+    (
+      <div className='tc'>
+        <h1 className='f1'>The Litter Box</h1>
+        <Searchbox searchChange = {this.onSearchChange}/>
+        <Scroll>
+          <CardList cats={filteredCats}/>
+        </Scroll>
+      </div>
+    );
+  }
+}
 export default App;
