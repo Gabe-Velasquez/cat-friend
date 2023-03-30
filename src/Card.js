2bf792947d8abd23775cb145acb0571f57666bd0
Gabe <gabriel.velasquez0116@gmail.com>
2023-03-30 17:54:14 -0600

    https

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({name, email, id}) => {
     return(
         <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-4'>
-            <img alt='cat' src={`http:robohash.org/${id}?set=set4/`} />
+            <img alt='cat' src={`https:robohash.org/${id}?set=set4/`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
