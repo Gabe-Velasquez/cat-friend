5a0b8fcec378753d2902b62b5ccc1d7eb912ab02
Gabe <gabriel.velasquez0116@gmail.com>
2023-03-31 11:09:11 -0600

    aligning img

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,9 @@ import React from "react";
 const Card = ({name, email, id}) => {
     return(
         <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-4'>
-            <img alt='cat' src={`https://robohash.org/${id}?set=set4/`} />
+            <img 
+            src={`https://robohash.org/${id}?set=set4/`}
+            alt='cat' />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
