2bd3646aaedaca2841a2fa6e43660f583a95a3e4
Gabe <gabriel.velasquez0116@gmail.com>
2023-04-03 13:45:31 -0600

    updating size of images on each card

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({name, email, id}) => {
     return(
         <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-4'>
             <img 
-            src= {`https://robohash.org/${id}?set=set4`}
+            src= {`https://robohash.org/${id}?set=set4?size=200x200`}
             alt='cat' />
             <div>
                 <h2>{name}</h2>
